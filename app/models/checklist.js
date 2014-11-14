2ec7a5a49b2ab02392f5119049e571799aa0bb6b
Eric Biesterfeld <ebiesterfeld@zipcar.com>
2014-11-14 14:53:14 -0500

    added badges to checklist

--- app/models/checklist.js
+++ app/models/checklist.js
@@ -3,15 +3,15 @@ import DS from 'ember-data';
 var Checklist = DS.Model.extend({
   vehicle: DS.belongsTo('Vehicle', {async: true}),
   damage_checked: DS.attr('boolean'),
-  damage_checked_by: DS.belongsTo('user'),
+  damage_checked_by: DS.attr(),
   cleaned: DS.attr('boolean'),
-  cleaned_by: DS.belongsTo('user'),
+  cleaned_by: DS.attr(),
   gas_checked: DS.attr('boolean'),
-  gas_checked_by: DS.belongsTo('user'),
+  gas_checked_by: DS.attr(),
   upkitted: DS.attr('boolean'),
-  upkitted_by: DS.belongsTo('user'),
+  upkitted_by: DS.attr(),
   parked_on_the_line: DS.attr('boolean'),
-  parked_on_the_line_by: DS.belongsTo('user'),
+  parked_on_the_line_by: DS.attr(),
 
   notReadyToToggle: function() {
     var readyToToggle = this.get('damage_checked') &&
