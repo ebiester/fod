15d96fd0f6fb02decbd5319058e9c2c53a37d2b1
Stuart Childs <schilds@zipcar.com>
2014-11-26 11:53:36 -0500

    Tests for checklist-item model.

--- tests/unit/models/checklist-item-test.js
+++ tests/unit/models/checklist-item-test.js
@@ -2,14 +2,21 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('checklist-item', 'ChecklistItem', {
-  // Specify the other units that are required for this test.
   needs: ['model:checklist', 'model:vehicle']
 });
 
 test('it exists', function() {
   var model = this.subject();
-  // var store = this.store();
   ok(!!model);
+});
+
+test('belongs to a checklist', function() {
+  var ChecklistItem = this.store().modelFor('checklist-item');
+  var relationship = Ember.get(ChecklistItem, 'relationshipsByName').get('checklist');
+
+  equal(relationship.key, 'checklist');
+  equal(relationship.kind, 'belongsTo');
 });
