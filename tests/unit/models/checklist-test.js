76bc77bce16f593442aacfd8a5389dbbc492f858
Stuart Childs <schilds@zipcar.com>
2014-11-26 11:51:42 -0500

    Tests for checklist model.

--- tests/unit/models/checklist-test.js
+++ tests/unit/models/checklist-test.js
@@ -2,14 +2,44 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('checklist', 'Checklist', {
-  // Specify the other units that are required for this test.
   needs: ['model:vehicle', 'model:checklist-item']
 });
 
 test('it exists', function() {
   var model = this.subject();
-  // var store = this.store();
   ok(!!model);
+});
+
+test('belongs to a vehicle', function() {
+  var Checklist = this.store().modelFor('checklist');
+  var relationship = Ember.get(Checklist, 'relationshipsByName').get('vehicle');
+
+  equal(relationship.key, 'vehicle');
+  equal(relationship.kind, 'belongsTo');
+});
+
+test('has a list of checklist items', function() {
+  var Checklist = this.store().modelFor('checklist');
+  var relationship = Ember.get(Checklist, 'relationshipsByName').get('checklistItems');
+
+  equal(relationship.key, 'checklistItems');
+  equal(relationship.kind, 'hasMany');
+});
+
+test('not ready when any item is incomplete', function() {
+  var item1, item2;
+  var store = this.store();
+  var checklist = this.subject();
+  Ember.run(function() {
+    item1 = store.createRecord('checklist-item', { checklist: checklist, complete: true });
+    item2 = store.createRecord('checklist-item', { checklist: checklist, complete: false });
+  });
+
+  equal(checklist.get('notReady'), true);
+
+  Ember.run(function() { item2.toggleProperty('complete'); });
+  equal(checklist.get('notReady'), false);
 });
